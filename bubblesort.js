096be7c3fab971853b7235bf339117f1dace5019
Joseph Knoebel <knoebelja@gmail.com>
2017-07-27 13:28:38 -0400

    sorry i forgot to commit Allie!!!

--- bubblesort.js
+++ bubblesort.js
@@ -1,7 +1,36 @@
 function bubbleSort(arr){
+  // keeps pushing largest number to the back
+  // after each go it doesn't look at the back
+  // always looks at two numbers next to eachother
+  // if we have a length of ten the first time will do 9 sorts
+
+  var length = arr.length-1;
+  for (var index = 0; index < arr.length; index++){
+      for (var i = 0; i < length; i++){
+      if (arr[i] > arr[i+1]){
+        // var tempVar;
+        // tempVar = arr[i+1];
+        // arr[i+1] = arr[i];
+        // arr[i] = tempVar;
+        arr = swap(arr, i);
+      }
+    }
+    length--;
+  }
+
   return arr;
 }
 
-bubbleSort.prototype.swap = function(){
+function swap(arr, i){
+  var tempVar;
+  tempVar = arr[i+1];
+  arr[i+1] = arr[i];
+  arr[i] = tempVar;
 
+  returns arr;
 }
+
+
+// bubbleSort.prototype.swap = function(){
+
+// }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,13 +1,27 @@
 describe('Bubble Sort', function(){
   it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+    expect( bubbleSort([1,2,3,4,5,6,7,8,9,10]) ).toEqual( [1,2,3,4,5,6,7,8,9,10] );
   });
 });
+
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([3,4,5,6,9,10,2,1,8,7]) ).toEqual( [1,2,3,4,5,6,7,8,9,10] );
+  });
+});
+
+
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([6,7,8,9,2,3,4,5]) ).toEqual( [2,3,4,5,6,7,8,9] );
+  });
+});
+
+
 before(function () {
-  spyOn(bubbleSort, "swap").and.callThrough(); // replace existing `tootsiepop['lick']` method
+  spyOn(bubbleSort, 'swap').and.callThrough(); // replace existing
 });
 it('Expect the swap counter to equal 100', function () {
-  var arr = [];
-  bubbleSort(arr);
-  expect(bubbleSort.swap.calls.count()).toEqual(100);
+  bubbleSort([3,4,5,6,9,10,2,1,8,7]);
+  expect(swap.calls.count()).toEqual(100);
 });
